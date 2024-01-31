a381ce15362be9e3c8e8c20e90bd41c04f7a9674
Serena Brizard <serenajolenebrizard@gmail.com>
2024-01-31 12:06:24 -0600

    Update oss.c
    notes for variable assignment

--- oss.c
+++ oss.c
@@ -1,1 +1,7 @@
+// oss -h -n proc -s simul -t iter
+// ex. oss -n 5 -s 3 -t 7
+// -h is help message
+// -n proc is the number of children to launch
+// -s simul is how many children can launch at the same time
+// -t iter is the number that have passed to user process
 
