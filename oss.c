3799c56c46be8a834cc9992ea9c10c65ebad006e
Serena Brizard <serenajolenebrizard@gmail.com>
2024-02-02 02:28:10 -0600

    Update oss.c

--- oss.c
+++ oss.c
@@ -5,3 +5,28 @@
 // -s simul is how many children can launch at the same time
 // -t iter is the number that have passed to user process
 
+const char optstr[] = "hr:c:d:";
+char opt;
+
+// getopt from OS_Tutorials.docx on Canvas
+while ( ( opt = getopt ( argc, argv, optstr ) ) != -1 )
+  {
+    switch (opt)
+    {
+      case 'h':
+        // Print help menu
+        break;
+      case 'n':
+        // n proc
+        break;
+      case 's':
+        // s simul
+        break;
+      case 't':
+        // t iter
+        break;
+      default:
+        printf ( "Invalid option %c\n", optopt );
+        break;
+    }
+  }
