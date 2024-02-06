66895a9fb95d8f0aa83d25e8deb45dde15b01392
Serena Brizard <serenajolenebrizard@gmail.com>
2024-02-05 23:18:20 -0600

    Update oss.c
    Fixed user menu to add all variables in option list

--- oss.c
+++ oss.c
@@ -2,13 +2,13 @@
 // PROJECT 1 - SKELETON MULTIPLE PROCESSES
 // UMSL CS 4760 - OPERATING SYSTEMS
 // TERM - SPRING 2024
+// OSS.C Resources - getopt(3) from OS_Tutorials.docx
 
 void helpmenu() // Input -h
   {
-  printf("Options:\n");
+  printf("-------------------HELP MENU-------------------\n");
   printf("Program will run using the following command structure:\n");
   printf("oss [-h] [-n proc] [-s simul] [-t iter]");
-    
     printf("[-h] will bring you to this screen, the help menu.  Will show you how to run this program.");
     printf("[-n proc] will begin to launch user processes. 'proc' is the number of times you want to launch.");
     printf("[-s simul] will control how many children are allowed to run simultaneously. 'simul' is the number of children allowed at once.");
@@ -19,19 +19,14 @@ void helpmenu() // Input -h
   printf("oss -n 5 -s 3 -t 7 \n");
   }
 
-
-// oss -h -n proc -s simul -t iter
-// ex. oss -n 5 -s 3 -t 7
-// -h is help message
-// -n proc is the number of children to launch
-// -s simul is how many children can launch at the same time
-// -t iter is the number that have passed to user process
-
-const char optstr[] = "hr:c:d:";
+const char optstr[] = "hn:s:t:";
 char opt;
+// Default values for variables
+proc = 0;
+simul = 0;
+iter = 0;
 
-// getopt from OS_Tutorials.docx on Canvas
-while ( ( opt = getopt ( argc, argv, optstr ) ) != -1 )
+while ((opt = getopt(argc, argv, optstr)) != -1)
   {
     switch (opt)
     {
@@ -40,15 +35,18 @@ while ( ( opt = getopt ( argc, argv, optstr ) ) != -1 )
         break;
       case 'n':
         // n proc
+        proc = atoi(optarg);
         break;
       case 's':
         // s simul
+        simul = atoi(optarg);
         break;
       case 't':
         // t iter
+        iter = atoi(optarg);
         break;
       default:
-        printf ( "Invalid option %c\n", optopt );
+        printf ("Invalid option %c\n", optopt);
         break;
     }
   }
